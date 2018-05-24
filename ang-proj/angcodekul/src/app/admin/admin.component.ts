bc547de5d6263248eaa7fb6af982f6850339181f
aniruddha <aniruddha.kudalkar@gmail.com>
2018-05-24 13:16:57 +0530

    readind the route params

--- ang-proj/angcodekul/src/app/admin/admin.component.ts
+++ ang-proj/angcodekul/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  adNm: string = 'default'
+
+  constructor(
+    private actRt: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-  }
+    console.log(this.actRt)
 
+    this.actRt.params.subscribe(
+      prm => {
+        this.adNm = prm['eml']
+      }
+    )
+  }
 }
